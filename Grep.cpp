8cd06855c9dcd0b5c73a2edfd5ce1acdae83b65c
Kumar Sanu <ksanu.37@gmail.com>
2020-06-10 19:37:00 +0530

    Update Grep.cpp

--- Grep.cpp
+++ Grep.cpp
@@ -46,7 +46,6 @@ int main(int argc, char* argv[]){
 	string pattern;
 	char* filename;
 	vector<char>options(4,0);
-	/*
 	if(argc==4){
 		pattern = convertToString(argv[2], sizeof(argv[2]) / sizeof(char));
 		filename = argv[3];
@@ -71,48 +70,12 @@ int main(int argc, char* argv[]){
 			}
 		}
 	}
-   	*/
-   
-   string input[4];
-   cout<<"Enter grep option pattern filename\n";
-   	
-   for(int i=0; i<4; i++){
-   	cin>>input[i];
-   } 
-   
-    //strcpy(filename, input[3].c_str()); 
 
    fstream file;
    vector<string>result;
    int count = 0;
-  
-
-
-   pattern = input[2];   /* Find out the pattern to be searched */
-   
-   for(int i=0; i<input[1].length(); i++){       /* getting all the options */
-   		if(input[1][i]!='-' && input[1][i]!=' '){
-   			switch(input[1][i]){
-   				case 'i': options[0] = 1;
-   						break;
-   				case 'v': options[1] = 1;
-   					break;
-   				case 'w' : options[2] = 1;
-   					break;
-   				case 'n' : options[3] = 1;
-   					break;
-			   }
-		   }
-		   
-		   cout<<"options set\n";	      
-   }
-   
-    cout<<pattern<<" ";
-   	string fileInput = input[3];	
-    file.open(fileInput.c_str(),ios::in); 
-   // cout<<input<<endl;
+   file.open(filename,ios::in); 
    if (file.is_open()){   //checking whether the file is open
-    cout<<"File opened\n";
       string line;
       while(getline(file, line)){ //read data from file object and put it into string.
 	         count++;
@@ -128,7 +91,6 @@ int main(int argc, char* argv[]){
 	         	
 	         	if(options[0]==1){
 				 	transform(line.begin(), line.end(), line.begin(), ::tolower); 
-					//transform(tempPattern.begin(), tempPattern.end(), tempPattern.begin(), ::tolower); 
 	         		check  = check & ignoreCase(line,pattern);
 	         	}
 	         	
@@ -138,8 +100,8 @@ int main(int argc, char* argv[]){
 	         	if(options[2]==1)
 	         		check = check & getPattern(line, pattern);
 	         		
-	         //	if(option[3]==1)
-	         //		temp = to_string(count)+": "+ temp;
+	            if(options[3]==1)
+	         		temp = to_string(count)+": "+ temp;
 
 				 
                  if(check)
@@ -148,10 +110,10 @@ int main(int argc, char* argv[]){
 
 		} 
     }
-      cout<<"################### RESULT ###################\n";
+    
+    // Print the result 
     for(int i=0; i<result.size(); i++)
       	cout<<result[i]<<"\n";
       	
     file.close(); //close the file object.
 }
-
