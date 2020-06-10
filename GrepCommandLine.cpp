a667566c7bb07d9f6ab6b416a851a373b464220e
Kumar Sanu <ksanu.37@gmail.com>
2020-06-10 19:34:42 +0530

    Update GrepCommandLine.cpp

--- GrepCommandLine.cpp
+++ GrepCommandLine.cpp
@@ -46,6 +46,7 @@ int main(int argc, char* argv[]){
 	string pattern;
 	char* filename;
 	vector<char>options(4,0);
+	/*
 	if(argc==4){
 		pattern = convertToString(argv[2], sizeof(argv[2]) / sizeof(char));
 		filename = argv[3];
@@ -70,11 +71,63 @@ int main(int argc, char* argv[]){
 			}
 		}
 	}
+   	*/
+    
+   cout<<"If you want to implement the default behaviour of grep (Without Options), Please enter Y, else enter N\n";
+
+   char userInput;
+   cin>>userInput;
+
+   string fileInput;
+   string input[4];
+
+   if(userInput == 'Y' || userInput== 'y'){
+   	cout<<"Enter the command\n";
+
+   	for(int i=0; i<3; i++){
+   		cin>>input[i];
+   	}
+
+   	pattern = input[1];
+   	fileInput = input[2];
+   	options.clear();
+   }
+   
+   else if(userInput=='N' || userInput=='n'){
+	   cout<<"Enter grep option pattern filename\n";
+	   	
+	   for(int i=0; i<4; i++){
+	   	cin>>input[i];
+	   } 
+	   
+	   fileInput = input[3];
+	   pattern = input[2];   /* Find out the pattern to be searched */
+	}
+	
+	else{
+	    cout<<"Please enter a valid input\n";
+	    return 0;
+	}
 
    fstream file;
    vector<string>result;
    int count = 0;
-   file.open(filename.c_str(),ios::in); 
+   
+   for(int i=0; i<input[1].length(); i++){       /* getting all the options */
+   		if(input[1][i]!='-' && input[1][i]!=' '){
+   			switch(input[1][i]){
+   				case 'i': options[0] = 1;
+   						break;
+   				case 'v': options[1] = 1;
+   					break;
+   				case 'w' : options[2] = 1;
+   					break;
+   				case 'n' : options[3] = 1;
+   					break;
+			   }
+		   }
+   }
+   file.open(fileInput.c_str(),ios::in); 
    if (file.is_open()){   //checking whether the file is open
       string line;
       while(getline(file, line)){ //read data from file object and put it into string.
@@ -91,6 +144,7 @@ int main(int argc, char* argv[]){
 	         	
 	         	if(options[0]==1){
 				 	transform(line.begin(), line.end(), line.begin(), ::tolower); 
+					//transform(tempPattern.begin(), tempPattern.end(), tempPattern.begin(), ::tolower); 
 	         		check  = check & ignoreCase(line,pattern);
 	         	}
 	         	
@@ -100,8 +154,8 @@ int main(int argc, char* argv[]){
 	         	if(options[2]==1)
 	         		check = check & getPattern(line, pattern);
 	         		
-	            if(option[3]==1)
-	         		temp = to_string(count)+": "+ temp;
+	            if(options[3]==1)
+	         	    temp = to_string(count)+": "+ temp;
 
 				 
                  if(check)
@@ -110,11 +164,9 @@ int main(int argc, char* argv[]){
 
 		} 
     }
-    
-    // Print the result 
+     
     for(int i=0; i<result.size(); i++)
       	cout<<result[i]<<"\n";
       	
     file.close(); //close the file object.
 }
-
